44a78c4a9d32217b196317bcbf7a1e9987dc28c1
Hanson Ma <hanson.hq.ma@gmail.com>
2022-08-15 13:51:22 -0400

    udp

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import socketserver
+import socket
 
-class handler(socketserver.BaseRequestHandler):
+host, port = "127.0.0.1", 9999
+buffersize = 1024 # 1g
+
+class TCPhandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         """
@@ -14,8 +18,20 @@ class handler(socketserver.BaseRequestHandler):
         self.request.sendall(data.upper())
 
 
-HOST, PORT = "127.0.0.1", 9999
 
-server = socketserver.TCPServer((HOST, PORT), handler)
+def spin_tcp_server(HOST, PORT):
 
-server.serve_forever()
+    server = socketserver.TCPServer((HOST, PORT), TCPhandler)
+
+    server.serve_forever()
+
+udpsocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+udpsocket.bind((host, port))
+
+while(True):
+    packet = udpsocket.recvfrom(buffersize)
+    msg = str(packet[0])
+    address = packet[1][0]
+    print("received packet from {}".format(address))
+    print("msg: {}".format(msg))
